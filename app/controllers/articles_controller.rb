3e5f57bb8bbe31967c894f0ac7bea52b878415ec
正木啓介 <kettan0130@gmail.com>
2021-10-20 08:20:55 +0900

    new-uiを作成

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -7,5 +7,12 @@ class ArticlesController < ApplicationController
         @article = Article.find(params[:id])
     end
 
+    def new
+        @article = Article.new
+    end
+
+    def create
+    end
+
    
 end
