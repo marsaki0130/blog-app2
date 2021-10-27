a8ace39833a091cb5fffb5e1e00f44caabb09695
正木啓介 <kettan0130@gmail.com>
2021-10-28 08:10:50 +0900

    画像アップロードと表示

--- app/controllers/profiles_controller.rb
+++ app/controllers/profiles_controller.rb
@@ -6,6 +6,30 @@ class ProfilesController < ApplicationController
     end
 
     def edit
-        @profile = current_user.build_profile #has_oneのとき、build_モデル名
+        @profile = current_user.prepare_profile
+    end
+
+    def update
+        @profile = current_user.prepare_profile
+        @profile.assign_attributes(profile_params)
+        if @profile.save
+          redirect_to profile_path, notice: 'プロフィールを更新しました'
+        else
+            flash.now[:error] = '更新できませんでした'
+            render :edit
+        end
+    end
+
+    private
+    def profile_params
+        params.require(:profile).permit(
+            :nickname,
+            :introduction,
+            :gender,
+            :birthday,
+            :subscribed,
+            :avatar
+
+        )
     end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -25,6 +25,8 @@ class User < ApplicationRecord
   has_many :articles, dependent: :destroy
   has_one :profile, dependent: :destroy
 
+  delegate :birthday, :age, :gender, to: :profile, allow_nil: true
+
   def has_written?(article)
     articles.exists?(id: article.id)
   end
@@ -32,6 +34,18 @@ class User < ApplicationRecord
   #user名を定義
 
   def display_name
-    self.email.split('@').first
+    profile&.nickname || self.email.split('@').first #ぼっち演算子
+  end
+
+  def prepare_profile
+    profile || build_profile
+  end
+
+  def avatar_image
+    if profile&.avatar&.attached?
+      profile.avatar
+    else
+      'default-avatar.png'
+    end
   end
 end
