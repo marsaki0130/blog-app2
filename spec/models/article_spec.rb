659c2606f375868a9aacf6f16e7b455d68b9d6de
正木啓介 <kettan0130@gmail.com>
2021-11-13 08:43:13 +0900

    factory bot 導入

--- spec/models/article_spec.rb
+++ spec/models/article_spec.rb
@@ -1,16 +1,20 @@
 require 'rails_helper'
 
 RSpec.describe Article, type: :model do
-  it 'タイトルと内容が入力されていれば、記事を保存できる' do 
-    user = User.create!({
-      email: 'test@example.com',
-      password: 'password'
-    })
-    article = user.articles.build({
-      title: Faker::Lorem.characters(number: 10),
-      content: Faker::Lorem.characters(number: 300)
-    })
+  let!(:user) { create (:user)}
 
-    expect(article).to be_valid
+
+  context 'タイトルと内容が入力されている場合' do
+    let!(:article) do
+      user.articles.build({
+        title: Faker::Lorem.characters(number: 10),
+        content: Faker::Lorem.characters(number: 300)
+      })
+    end
+    
+    it '記事を保存できる' do
+      puts user.email  
+      expect(article).to be_valid
+    end
   end
 end
